5ba5ea386b1d2f50dea598a4cfd48d7ce33ac54d
hritik79 <62216626+hritik79@users.noreply.github.com>
2022-04-17 17:40:25 +0530

    First Commit

---
+++ Day7_this_static_instanceof/src/com/indiabix/static1/Example3.java
@@ -0,0 +1,17 @@
+package com.indiabix.static1;
+
+public class Example3 
+{
+	static int x;
+	static String str;
+	static
+	{
+		x=10;
+		str="Capgemini";
+	}
+	public static void main(String[] args) {
+		System.out.println(x);
+		System.out.println(str);
+	}
+
+}
