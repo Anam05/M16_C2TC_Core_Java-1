c15b7aa6e4ba1bc0eb50b28136ca44d65f602e5e
hritik79 <62216626+hritik79@users.noreply.github.com>
2022-05-02 19:51:27 +0530

    First Commit

---
+++ Day15_Collection1.1_ArrayList_Linked_list/src/com/indiabix/collections/Example5.java
@@ -0,0 +1,15 @@
+package com.indiabix.collections;
+
+import java.util.Vector;
+
+public class Example5 {
+
+	public static void main(String[] args) {
+		Vector v= new Vector();
+		v.add("Hritik");
+		v.add(11);
+		System.out.println(v);
+
+	}
+
+}
