769c21d636e1a3d06897ba1528574334c1a06189
hritik79 <62216626+hritik79@users.noreply.github.com>
2022-05-19 16:29:50 +0530

    First Commit

---
+++ Day25_Enum and Regex/src/com/indiabix/regex/Example8.java
@@ -0,0 +1,21 @@
+package com.indiabix.regex;
+
+import java.util.Scanner;
+
+public class Example8 {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Scanner s=new Scanner(System.in);
+		String str="M-16 @@ Pratik Jain @@ Ankita @@ Hritik";
+		String[] res=str.split("@@");
+		for(String i:res)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
+
